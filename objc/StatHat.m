c9f0274740f81a44abf9a7bf16e870165f00b77b
Valeriy Dyryavyy <valeriy.dyryavyy@corp.badoo.com>
2013-06-13 17:43:52 +0100

    Fixes crash caused by not checking the data length which comes from the server, when it's empty app crashes

--- objc/StatHat.m
+++ objc/StatHat.m
@@ -22,21 +22,21 @@
 
 + (StatHat*)postEZStat:(NSString*)statName withValue:(double)value forUser:(NSString*)ezkey delegate:(id<StatHatDelegate>)delegate
 {
-        StatHat* sh = [[[StatHat alloc] init] autorelease];
-        sh.delegate = delegate;
+    StatHat* sh = [[[StatHat alloc] init] autorelease];
+    sh.delegate = delegate;
 
-        [sh ezPostStat:statName withValue:value forUser:ezkey];
-        return sh;
+    [sh ezPostStat:statName withValue:value forUser:ezkey];
+    return sh;
 }
 
 + (StatHat*)postEZStat:(NSString*)statName withCount:(double)count forUser:(NSString*)ezkey delegate:(id<StatHatDelegate>)delegate
 {
-        StatHat* sh = [[[StatHat alloc] init] autorelease];
-        sh.delegate = delegate;
+    StatHat* sh = [[[StatHat alloc] init] autorelease];
+    sh.delegate = delegate;
 
-        [sh ezPostStat:statName withCount:count forUser:ezkey];
+    [sh ezPostStat:statName withCount:count forUser:ezkey];
 
-        return sh;
+    return sh;
 }
 
 //
@@ -45,20 +45,20 @@
 
 + (StatHat*)postStatKey:(NSString*)statKey withValue:(double)value forUser:(NSString*)userKey delegate:(id<StatHatDelegate>)delegate
 {
-        StatHat* sh = [[[StatHat alloc] init] autorelease];
-        sh.delegate = delegate;
-        [sh postStatKey:statKey withValue:value forUserKey:userKey];
-        return sh;
+    StatHat* sh = [[[StatHat alloc] init] autorelease];
+    sh.delegate = delegate;
+    [sh postStatKey:statKey withValue:value forUserKey:userKey];
+    return sh;
 }
 
 + (StatHat*)postStatKey:(NSString*)statKey withCount:(double)count forUser:(NSString*)userKey delegate:(id<StatHatDelegate>)delegate
 {
-        StatHat* sh = [[[StatHat alloc] init] autorelease];
-        sh.delegate = delegate;
+    StatHat* sh = [[[StatHat alloc] init] autorelease];
+    sh.delegate = delegate;
 
-        [sh postStatKey:statKey withCount:count forUserKey:userKey];
+    [sh postStatKey:statKey withCount:count forUserKey:userKey];
 
-        return sh;
+    return sh;
 }
 
 //
@@ -69,41 +69,41 @@
 
 - (id)init
 {
-        if ((self = [super init]) == nil) {
-                return nil;
-        }
+    if ((self = [super init]) == nil) {
+        return nil;
+    }
 
-        _body = [[NSMutableString alloc] init];
+    _body = [[NSMutableString alloc] init];
 
-        return self;
+    return self;
 }
 
 - (void)ezPostStat:(NSString*)statName withValue:(double)value forUser:(NSString*)ezkey
 {
-        NSMutableDictionary* params = [self makeEZParamsForStat:statName user:ezkey];
-        [params setObject:[NSNumber numberWithDouble:value] forKey:@"value"];
-        [self httpPostDict:params toPath:@"ez"];
+    NSMutableDictionary* params = [self makeEZParamsForStat:statName user:ezkey];
+    [params setObject:[NSNumber numberWithDouble:value] forKey:@"value"];
+    [self httpPostDict:params toPath:@"ez"];
 }
 
 - (void)ezPostStat:(NSString*)statName withCount:(double)count forUser:(NSString*)ezkey
 {
-        NSMutableDictionary* params = [self makeEZParamsForStat:statName user:ezkey];
-        [params setObject:[NSNumber numberWithDouble:count] forKey:@"count"];
-        [self httpPostDict:params toPath:@"ez"];
+    NSMutableDictionary* params = [self makeEZParamsForStat:statName user:ezkey];
+    [params setObject:[NSNumber numberWithDouble:count] forKey:@"count"];
+    [self httpPostDict:params toPath:@"ez"];
 }
 
 - (void)postStatKey:(NSString*)statKey withValue:(double)value forUserKey:(NSString*)userKey
 {
-        NSMutableDictionary* params = [self makeClassicParamsForStat:statKey user:userKey];
-        [params setObject:[NSNumber numberWithDouble:value] forKey:@"value"];
-        [self httpPostDict:params toPath:@"v"];
+    NSMutableDictionary* params = [self makeClassicParamsForStat:statKey user:userKey];
+    [params setObject:[NSNumber numberWithDouble:value] forKey:@"value"];
+    [self httpPostDict:params toPath:@"v"];
 }
 
 - (void)postStatKey:(NSString*)statKey withCount:(double)count forUserKey:(NSString*)userKey
 {
-        NSMutableDictionary* params = [self makeClassicParamsForStat:statKey user:userKey];
-        [params setObject:[NSNumber numberWithDouble:count] forKey:@"count"];
-        [self httpPostDict:params toPath:@"c"];
+    NSMutableDictionary* params = [self makeClassicParamsForStat:statKey user:userKey];
+    [params setObject:[NSNumber numberWithDouble:count] forKey:@"count"];
+    [self httpPostDict:params toPath:@"c"];
 }
 
 //
@@ -112,40 +112,45 @@
 
 - (void)connection:(NSURLConnection*)connection didReceiveData:(NSData*)data
 {
-        if (connection != _connection) {
-                return;
-        }
+    if (connection != _connection) {
+        return;
+    }
 
-        [_body appendString:[NSString stringWithUTF8String:[data bytes]]];
+    if (data.length > 0) {
+        NSString *dataString = [NSString stringWithUTF8String:[data bytes]];
+        if (dataString.length > 0) {
+            [_body appendString:dataString];
+        }
+    }
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
 {
-        if (connection != _connection) {
-                return;
-        }
+    if (connection != _connection) {
+        return;
+    }
 
-        [self releaseConnection];
+    [self releaseConnection];
 
-        [_delegate statHat:self postError:error];
+    [_delegate statHat:self postError:error];
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
-        if (connection != _connection) {
-                return;
-        }
+    if (connection != _connection) {
+        return;
+    }
 
-        [self releaseConnection];
+    [self releaseConnection];
 
-        [_delegate statHat:self postFinished:_body];
+    [_delegate statHat:self postFinished:_body];
 }
 
 - (void)dealloc
 {
-        [_connection release];
-        [_body release];
-        [super dealloc];
+    [_connection release];
+    [_body release];
+    [super dealloc];
 }
 
 @end
@@ -157,43 +162,43 @@
 
 - (void)httpPostDict:(NSDictionary*)params toPath:(NSString*)path
 {
-        NSString* url = [NSString stringWithFormat:@"http://api.stathat.com/%@", path];
-        NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+    NSString* url = [NSString stringWithFormat:@"http://api.stathat.com/%@", path];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
 
-        NSMutableArray* dataPairs = [NSMutableArray array];
-        for (NSString* key in [params allKeys]) {
-                NSString* pair = [NSString stringWithFormat:@"%@=%@", key, [params objectForKey:key]];
-                [dataPairs addObject:pair];
-        }
+    NSMutableArray* dataPairs = [NSMutableArray array];
+    for (NSString* key in [params allKeys]) {
+        NSString* pair = [NSString stringWithFormat:@"%@=%@", key, [params objectForKey:key]];
+        [dataPairs addObject:pair];
+    }
 
-        NSString* dataStr = [dataPairs componentsJoinedByString:@"&"];
+    NSString* dataStr = [dataPairs componentsJoinedByString:@"&"];
 
-        [request setHTTPMethod:@"POST"];
-        [request setHTTPBody:[dataStr dataUsingEncoding:NSUTF8StringEncoding]];
-        _connection = [[NSURLConnection connectionWithRequest:request delegate:self] retain];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:[dataStr dataUsingEncoding:NSUTF8StringEncoding]];
+    _connection = [[NSURLConnection connectionWithRequest:request delegate:self] retain];
 }
 
 
 - (NSMutableDictionary*)makeEZParamsForStat:(NSString*) statName user:(NSString*)ezkey
 {
-        NSMutableDictionary* params = [NSMutableDictionary dictionary];
-        [params setObject:ezkey forKey:@"ezkey"];
-        [params setObject:statName forKey:@"stat"];
-        return params;
+    NSMutableDictionary* params = [NSMutableDictionary dictionary];
+    [params setObject:ezkey forKey:@"ezkey"];
+    [params setObject:statName forKey:@"stat"];
+    return params;
 }
 
 - (NSMutableDictionary*)makeClassicParamsForStat:(NSString*) statKey user:(NSString*)userKey
 {
-        NSMutableDictionary* params = [NSMutableDictionary dictionary];
-        [params setObject:statKey forKey:@"key"];
-        [params setObject:userKey forKey:@"ukey"];
-        return params;
+    NSMutableDictionary* params = [NSMutableDictionary dictionary];
+    [params setObject:statKey forKey:@"key"];
+    [params setObject:userKey forKey:@"ukey"];
+    return params;
 }
 
 - (void)releaseConnection
 {
-        [_connection release];
-        _connection = nil;
+    [_connection release];
+    _connection = nil;
 }
 
 @end
